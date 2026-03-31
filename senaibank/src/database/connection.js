49a49817d3a7c551ada592b8b03126dbf3ed3f8c
sammyrasa <samyra.negreiros09@gmail.com>
2026-03-31 09:13:24 -0300

    conectei com o banco

--- senaibank/src/database/connection.js
+++ senaibank/src/database/connection.js
@@ -1,1 +1,20 @@
 // Conexão (SQLite/MySQL) que você mostrou
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "senai",
+  database: "senaibank"
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar:", err);
+    return;
+  }
+
+  console.log("Conectado ao MySQL");
+});
+
+module.exports = connection;
