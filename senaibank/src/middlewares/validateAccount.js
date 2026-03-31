1c1c648c520969fdd768c137d601776c57ab6f45
leonardobellorin568-cmd <leonardobellorin568@gmail.com>
2026-03-31 10:00:24 -0300

    Update validateAccount.js

--- senaibank/src/middlewares/validateAccount.js
+++ senaibank/src/middlewares/validateAccount.js
@@ -1,1 +1,30 @@
-// Valida se CPF já existe ou campos vazios
+const Account = require('../models/Account');
+
+async function validateAccount(req, res, next) {
+    const{ nome, cpf, balance } = req.body; 
+
+    if(!nome || !cpf || balance === undefined) {
+        return res.status(400).json({
+            error: 'Campos obrigatorios nao preenchidos'
+        });
+    }
+
+    try{
+        const existingAccount = await
+Account.findOne({ cpf});
+
+        if (existingAccount) {
+            return res.status(400).json({
+                error: ' CPF ja cadastrado'
+            });
+        }
+
+        next();
+    } catch (err) {
+        return res.status(500).json({
+            error: 'Erro interno no servidor'
+        });
+    }
+}
+
+module.exports = validateAccount;
